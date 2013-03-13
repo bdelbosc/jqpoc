2a015e880a474663bdd5f66f53b6546afba8a57c
bdelbosc <bdelbosc@nuxeo.com>
2013-03-13 23:41:41 +0100

    Add metrics

--- src/main/java/org/nuxeo/ecm/platform/jqpoc/Consumer.java
+++ src/main/java/org/nuxeo/ecm/platform/jqpoc/Consumer.java
@@ -7,26 +7,34 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Timer;
+import com.yammer.metrics.core.TimerContext;
 import com.yammer.metrics.graphite.GraphiteReporter;
 
 public class Consumer {
     private static final Log log = LogFactory.getLog(Consumer.class);
 
-    private static final int NTHREDS = 20;
+    private static final int NTHREDS = 6;
 
     private static final String QUEUE_NAME = "q";
 
     private static final long DELAY_MS = 0;
 
+    protected final static Timer consumerTimer = Metrics.defaultRegistry().newTimer(
+            Producer.class, "consumer", TimeUnit.MICROSECONDS, TimeUnit.SECONDS);
+
     public static void main(String[] args) {
-        GraphiteReporter.enable(1, TimeUnit.SECONDS, "localhost",
-                2030, "java.strix.");
+        GraphiteReporter.enable(1, TimeUnit.SECONDS, "localhost", 2030,
+                "java.strix.");
 
         ExecutorService executor = Executors.newFixedThreadPool(NTHREDS);
 
         JobQueue queue = new JobQueue(QUEUE_NAME, 10);
+        long size = queue.getSize();
         log.info("Starting " + NTHREDS + " threads of consumers, pending job: "
-                + queue.getSize());
+                + size);
+        TimerContext clock = consumerTimer.time();
 
         for (int i = 0; i < NTHREDS; i++) {
             Runnable worker = new ConsumerRunnable(QUEUE_NAME, DELAY_MS);
@@ -36,8 +44,11 @@ public class Consumer {
         executor.shutdown();
         while (!executor.isTerminated()) {
         }
-        log.info("All consumers finished, queue size=" + queue.getSize());
-
+        long elapsed = clock.stop();
+                double rate = (size / (double) elapsed) * 1000000000;
+        log.info(String.format(
+                "All consumers finished, queue size=%d rate=%.0f j/s elapsed=%.3f s",
+                queue.getSize(), rate, elapsed / (double) 1000000000));
     }
 
 }
--- src/main/java/org/nuxeo/ecm/platform/jqpoc/Producer.java
+++ src/main/java/org/nuxeo/ecm/platform/jqpoc/Producer.java
@@ -17,7 +17,7 @@ public class Producer {
 
     private static final int NJOBS = 1000000;
 
-    private static final int NTHREDS = 10;
+    private static final int NTHREDS = 6;
 
     private static final int TIMEOUT = 10;
 
@@ -26,31 +26,35 @@ public class Producer {
     private static final String PREFIX_JID = "t";
 
     protected final static Timer producerTimer = Metrics.defaultRegistry().newTimer(
-            Producer.class, "producer",
-            TimeUnit.MICROSECONDS, TimeUnit.SECONDS);
+            Producer.class, "producer", TimeUnit.MICROSECONDS, TimeUnit.SECONDS);
 
     public static void main(String[] args) {
 
-        GraphiteReporter.enable(1, TimeUnit.SECONDS, "localhost",
-               2030, "java.strix.");
+        GraphiteReporter.enable(1, TimeUnit.SECONDS, "localhost", 2030,
+                "java.strix.");
 
         ExecutorService executor = Executors.newFixedThreadPool(NTHREDS);
         log.info("Starting " + NTHREDS + " threads of producers");
-        // raz the queue
+
         JobQueue q = new JobQueue(QUEUE_NAME, TIMEOUT);
         q.flush();
         TimerContext clock = producerTimer.time();
         for (int i = 0; i < NTHREDS; i++) {
             Runnable worker = new ProducerRunnable(PREFIX_JID
-                    + Integer.valueOf(i).toString(), QUEUE_NAME, (long) (NJOBS / NTHREDS));
+                    + Integer.valueOf(i).toString(), QUEUE_NAME,
+                    (long) (NJOBS / NTHREDS));
             executor.execute(worker);
         }
         log.info("Producer running");
         executor.shutdown();
         while (!executor.isTerminated()) {
         }
-        clock.stop();
-        log.info("All producer finished, queue size=" + q.getSize());
+        long elapsed = clock.stop();
+        log.info(elapsed);
+        double rate = (NJOBS / (double) elapsed) * 1000000000;
+        log.info(String.format(
+                "All producer finished, queue size=%d rate=%.0f j/s elapsed=%.3f s",
+                q.getSize(), rate, elapsed / (double) 1000000000));
         q.close();
     }
 
